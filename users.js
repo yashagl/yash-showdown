5b12f8a1b12025acbc9f58a60f200891c5935cd5
awolffromspace <ewolft@gmail.com>
2015-08-12 16:55:38 -0400

    Add new version of tell command

--- users.js
+++ users.js
@@ -808,6 +808,7 @@ User = (function () {
 			this.send('|nametaken|' + name + "|Your authentication token was invalid.");
 		}
 
+		if (Tells.inbox[userid]) Tells.sendTell(userid, this);
 		return false;
 	};
 	User.prototype.validateRename = function (name, tokenData, newlyRegistered, challenge) {
