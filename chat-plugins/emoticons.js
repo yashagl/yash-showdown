e9e85bcbd8954951b80f5ec3674f0eb13a2bba0a
awolffromspace <ewolft@gmail.com>
2015-08-16 21:54:33 -0400

    Updates

---
+++ chat-plugins/emoticons.js
@@ -0,0 +1,212 @@
+var color = require('../config/color');
+
+exports.parseEmoticons = parseEmoticons;
+
+var emotes = {
+	':absol:': 'http://cbc.pokecommunity.com/config/emoticons/absol.png',
+	':arceus:': 'http://cbc.pokecommunity.com/config/emoticons/arceus.png',
+	':armycat:': 'http://cbc.pokecommunity.com/config/emoticons/armycat.png',
+	':azelf:': 'http://cbc.pokecommunity.com/config/emoticons/azelf.png',
+	':bidoof:': 'http://cbc.pokecommunity.com/config/emoticons/bidoof.png',
+	':bye:': 'http://cbc.pokecommunity.com/config/emoticons/bye.gif',
+	':castform:': 'http://cbc.pokecommunity.com/config/emoticons/castform.png',
+	':catflip:': 'http://cbc.pokecommunity.com/config/emoticons/catflip.png',
+	':charizard:': 'http://cbc.pokecommunity.com/config/emoticons/charizard.png',
+	':clown:': 'http://cbc.pokecommunity.com/config/emoticons/clown.png',
+	':cookie:': 'http://cbc.pokecommunity.com/config/emoticons/cookie.png',
+	':dk:': 'http://cbc.pokecommunity.com/config/emoticons/dk.png',
+	':electrode:': 'http://cbc.pokecommunity.com/config/emoticons/electrode.png',
+	':espurr:': 'http://cbc.pokecommunity.com/config/emoticons/espurr.png',
+	':flirt:': 'http://cbc.pokecommunity.com/config/emoticons/flirt.png',
+	':gav:': 'http://cbc.pokecommunity.com/config/emoticons/gav.png',
+	':gloom:': 'http://cbc.pokecommunity.com/config/emoticons/gloom.png',
+	':growlithe:': 'http://cbc.pokecommunity.com/config/emoticons/growlithe.png',
+	':hamster:': 'http://cbc.pokecommunity.com/config/emoticons/hamster.png',
+	':helix:': 'http://cbc.pokecommunity.com/config/emoticons/helix.png',
+	':houndoom:': 'http://cbc.pokecommunity.com/config/emoticons/houndoom.png',
+	':jigglypuff:': 'http://cbc.pokecommunity.com/config/emoticons/jigglypuff.png',
+	':jynx:': 'http://cbc.pokecommunity.com/config/emoticons/jynx.png',
+	':kappa:': 'http://cbc.pokecommunity.com/config/emoticons/kappa.png',
+	':keepo:': 'http://cbc.pokecommunity.com/config/emoticons/keepo.png',
+	':kermit:': 'http://cbc.pokecommunity.com/config/emoticons/kermit.png',
+	':kreygasm:': 'http://cbc.pokecommunity.com/config/emoticons/kreygasm.png',
+	':lapras:': 'http://cbc.pokecommunity.com/config/emoticons/lapras.png',
+	':lileep:': 'http://cbc.pokecommunity.com/config/emoticons/lileep.png',
+	':ludicolo:': 'http://cbc.pokecommunity.com/config/emoticons/ludicolo.png',
+	':luvdisc:': 'http://cbc.pokecommunity.com/config/emoticons/luvdisc.png',
+	':magikarp:': 'http://cbc.pokecommunity.com/config/emoticons/magikarp.png',
+	':meganium:': 'http://cbc.pokecommunity.com/config/emoticons/meganium.png',
+	':meowstic:': 'http://cbc.pokecommunity.com/config/emoticons/meowstic.png',
+	':meowsticf:': 'http://cbc.pokecommunity.com/config/emoticons/meowsticf.png',
+	':metagross:': 'http://cbc.pokecommunity.com/config/emoticons/metagross.png',
+	':moo:': 'http://cbc.pokecommunity.com/config/emoticons/moo.gif',
+	':nw:': 'http://cbc.pokecommunity.com/config/emoticons/nw.gif',
+	':oddish:': 'http://cbc.pokecommunity.com/config/emoticons/oddish.png',
+	':pear:': 'http://cbc.pokecommunity.com/config/emoticons/pear.png',
+	':pjsalt:': 'http://cbc.pokecommunity.com/config/emoticons/pjsalt.png',
+	':pogchamp:': 'http://cbc.pokecommunity.com/config/emoticons/pogchamp.png',
+	':potato:': 'http://cbc.pokecommunity.com/config/emoticons/potato.png',
+	':psyduck:': 'http://cbc.pokecommunity.com/config/emoticons/psyduck.png',
+	':pyoshi:': 'http://cbc.pokecommunity.com/config/emoticons/pyoshi.png',
+	':seduce:': 'http://cbc.pokecommunity.com/config/emoticons/seduce.png',
+	':senpai:': 'http://cbc.pokecommunity.com/config/emoticons/senpai.png',
+	':sims:': 'http://cbc.pokecommunity.com/config/emoticons/sims.png',
+	':slowpoke:': 'http://cbc.pokecommunity.com/config/emoticons/slowpoke.png',
+	':snorlax:': 'http://cbc.pokecommunity.com/config/emoticons/snorlax.png',
+	':spheal:': 'http://cbc.pokecommunity.com/config/emoticons/spheal.png',
+	':sri:': 'http://cbc.pokecommunity.com/config/emoticons/sri.png',
+	':strut:': 'http://cbc.pokecommunity.com/config/emoticons/strut.png',
+	':suicune:': 'http://cbc.pokecommunity.com/config/emoticons/suicune.png',
+	':superman:': 'http://cbc.pokecommunity.com/config/emoticons/superman.png',
+	':sweep:': 'http://cbc.pokecommunity.com/config/emoticons/sweep.gif',
+	':taco:': 'http://cbc.pokecommunity.com/config/emoticons/taco.png',
+	':vulpix:': 'http://cbc.pokecommunity.com/config/emoticons/vulpix.png',
+	':weezing:': 'http://cbc.pokecommunity.com/config/emoticons/weezing.png',
+	':why:': 'http://cbc.pokecommunity.com/config/emoticons/why.png',
+	':wobbuffet:': 'http://cbc.pokecommunity.com/config/emoticons/wobbuffet.png',
+	':wooper:': 'http://cbc.pokecommunity.com/config/emoticons/wooper.png',
+	':wynaut:': 'http://cbc.pokecommunity.com/config/emoticons/wynaut.png',
+	':y:': 'http://cbc.pokecommunity.com/config/emoticons/y.png',
+	':yoshi:': 'http://cbc.pokecommunity.com/config/emoticons/yoshi.png'
+};
+
+var emotesKeys = Object.keys(emotes);
+var patterns = [];
+var metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
+
+for (var i in emotes) {
+	if (emotes.hasOwnProperty(i)) {
+		patterns.push('(' + i.replace(metachars, '\\$&') + ')');
+	}
+}
+var patternRegex = new RegExp(patterns.join('|'), 'g');
+
+/**
+ * Parse emoticons in message.
+ *
+ * @param {String} message
+ * @param {Object} room
+ * @param {Object} user
+ * @param {Boolean} pm - returns a string if it is in private messages
+ * @returns {Boolean|String}
+ */
+function parseEmoticons(message, room, user, pm) {
+	if (typeof message !== 'string' || (!pm && room.disableEmoticons)) return false;
+
+	var match = false;
+	var len = emotesKeys.length;
+
+
+	while (len--) {
+		if (message && message.indexOf(emotesKeys[len]) >= 0) {
+			match = true;
+			break;
+		}
+	}
+
+	if (!match) return false;
+
+	// escape HTML
+	message = Tools.escapeHTML(message);
+
+	// add emotes
+	message = message.replace(patternRegex, function (match) {
+		var emote = emotes[match];
+		return typeof emote === 'string' ? '<img src="' + emote + '" title="' + match + '" />' : match;
+	});
+
+	// __italics__
+	message = message.replace(/\_\_([^< ](?:[^<]*?[^< ])?)\_\_(?![^<]*?<\/a)/g, '<i>$1</i>');
+
+	// **bold**
+	message = message.replace(/\*\*([^< ](?:[^<]*?[^< ])?)\*\*/g, '<b>$1</b>');
+
+	var group = user.getIdentity().charAt(0);
+	if (room.auth) group = room.auth[user.userid] || group;
+
+	var style = "background:none;border:0;padding:0 5px 0 0;font-family:Verdana,Helvetica,Arial,sans-serif;font-size:9pt;cursor:pointer";
+
+	message = "<div class='chat'>" + "<small>" + group + "</small>" + "<button name='parseCommand' value='/user " + user.name + "' style='" + style + "'>" + "<b><font color='" + color(user.userid) + "'>" + user.name + ":</font></b>" + "</button><em class='mine'>" + message + "</em></div>";
+	if (pm) return message;
+
+	room.addRaw(message);
+
+	return true;
+}
+
+/**
+ * Create a two column table listing emoticons.
+ *
+ * @return {String} emotes table
+ */
+function create_table() {
+	var emotes_name = Object.keys(emotes);
+	var emotes_list = [];
+	var emotes_group_list = [];
+	var len = emotes_name.length;
+
+	for (var i = 0; i < len; i++) {
+		emotes_list.push("<td>" +
+			"<img src='" + emotes[emotes_name[i]] + "'' title='" + emotes_name[i] + "'>" +
+			emotes_name[i] + "</td>");
+	}
+
+	var emotes_list_right = emotes_list.splice(len / 2, len / 2);
+
+	for (var i = 0; i < len / 2; i++) {
+		var emote1 = emotes_list[i],
+			emote2 = emotes_list_right[i];
+		if (emote2) {
+			emotes_group_list.push("<tr>" + emote1 + emote2 + "</tr>");
+		} else {
+			emotes_group_list.push("<tr>" + emote1 + "</tr>");
+		}
+	}
+
+	return "<center><b><u>List of Emoticons</u></b></center>" + "<table border='1' cellspacing='0' cellpadding='5' width='100%'>" + "<tbody>" + emotes_group_list.join("") + "</tbody>" + "</table>";
+}
+
+var emotes_table = create_table();
+
+exports.commands = {
+	blockemote: 'blockemoticons',
+	blockemotes: 'blockemoticons',
+	blockemoticon: 'blockemoticons',
+	blockemoticons: function (target, room, user) {
+		if (user.blockEmoticons === (target || true)) return this.sendReply("You are already blocking emoticons in private messages! To unblock, use /unblockemoticons");
+		user.blockEmoticons = true;
+		return this.sendReply("You are now blocking emoticons in private messages.");
+	},
+	blockemoticonshelp: ["/blockemoticons - Blocks emoticons in private messages. Unblock them with /unblockemoticons."],
+
+	unblockemote: 'unblockemoticons',
+	unblockemotes: 'unblockemoticons',
+	unblockemoticon: 'unblockemoticons',
+	unblockemoticons: function (target, room, user) {
+		if (!user.blockEmoticons) return this.sendReply("You are not blocking emoticons in private messages! To block, use /blockemoticons");
+		user.blockEmoticons = false;
+		return this.sendReply("You are no longer blocking emoticons in private messages.");
+	},
+	unblockemoticonshelp: ["/unblockemoticons - Unblocks emoticons in private messages. Block them with /blockemoticons."],
+
+	emotes: 'emoticons',
+	emoticons: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		this.sendReply("|raw|" + emotes_table);
+	},
+	emoticonshelp: ["/emoticons - Get a list of emoticons."],
+
+	toggleemote: 'toggleemoticons',
+	toggleemotes: 'toggleemoticons',
+	toggleemoticons: function (target, room, user) {
+		if (!this.can('declare', null, room)) return false;
+		room.disableEmoticons = !room.disableEmoticons;
+		this.sendReply("Disallowing emoticons is set to " + room.disableEmoticons + " in this room.");
+		if (room.disableEmoticons) {
+			this.add("|raw|<div class=\"broadcast-red\"><b>Emoticons are disabled!</b><br />Emoticons will not work.</div>");
+		} else {
+			this.add("|raw|<div class=\"broadcast-blue\"><b>Emoticons are enabled!</b><br />Emoticons will work now.</div>");
+		}
+	},
+	toggleemoticonshelp: ["/toggleemoticons - Toggle emoticons on or off."]
+};
