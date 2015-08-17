5cf0ce953cfed57bd8b3082a718510566d93b3e3
awolffromspace <ewolft@gmail.com>
2015-08-16 21:56:42 -0400

    Update emoticons

--- command-parser.js
+++ command-parser.js
@@ -36,6 +36,7 @@ const BROADCAST_TOKEN = '!';
 
 var fs = require('fs');
 var path = require('path');
+var parseEmoticons = require('./chat-plugins/emoticons').parseEmoticons;
 
 /*********************************************************
  * Load command files
@@ -532,7 +533,7 @@ var parse = exports.parse = function (message, room, user, connection, levelsDee
 
 	message = canTalk.call(context, user, room, connection, message);
 
-	if (!Core.processChatData(user, room, connection, message)) return false;
+	if (parseEmoticons(message, room, user)) return;
 
 	return message || false;
 };
