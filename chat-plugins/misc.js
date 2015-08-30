77ed1d330e1628f2ba135f43d2afad734fc50dfd
awolffromspace <ewolft@gmail.com>
2015-08-30 09:18:04 -0400

    Update misc.js

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -6,49 +6,7 @@ var fs = require('fs');
 var moment = require('moment');
 var request = require('request');
 
-var messages = [
-	"has vanished into nothingness!",
-	"used Explosion!",
-	"fell into the void.",
-	"went into a cave without a repel!",
-	"has left the building.",
-	"was forced to give StevoDuhHero's mom an oil massage!",
-	"was hit by Magikarp's Revenge!",
-	"ate a bomb!",
-	"is blasting off again!",
-	"(Quit: oh god how did this get here i am not good with computer)",
-	"was unfortunate and didn't get a cool message.",
-	"{{user}}'s mama accidently kicked {{user}} from the server!"
-];
-
 exports.commands = {
-	stafflist: 'authority',
-	auth: 'authority',
-	authlist: 'authority',
-	authority: function (target, room, user, connection) {
-		var rankLists = {};
-		var ranks = Object.keys(Config.groups);
-		for (var u in Users.usergroups) {
-			var rank = Users.usergroups[u].charAt(0);
-			// In case the usergroups.csv file is not proper, we check for the server ranks.
-			if (ranks.indexOf(rank) > -1) {
-				var name = Users.usergroups[u].substr(1);
-				if (!rankLists[rank]) rankLists[rank] = [];
-				if (name) rankLists[rank].push(((Users.getExact(name) && Users.getExact(name).connected) ? '**' + name + '**' : name));
-			}
-		}
-
-		var buffer = [];
-		Object.keys(rankLists).sort(function (a, b) {
-			return (Config.groups[b] || {rank: 0}).rank - (Config.groups[a] || {rank: 0}).rank;
-		}).forEach(function (r) {
-			buffer.push((Config.groups[r] ? r + Config.groups[r].name + "s (" + rankLists[r].length + ")" : r) + ":\n" + rankLists[r].sort().join(", "));
-		});
-
-		if (!buffer.length) buffer = "This server has no auth.";
-		connection.popup(buffer.join("\n\n"));
-	},
-
 	clearall: function (target, room, user) {
 		if (!this.can('declare')) return false;
 		if (room.battle) return this.sendReply("You cannot clearall in battle rooms.");
@@ -69,32 +27,6 @@ exports.commands = {
 			}
 		}, 1000);
 	},
-
-	hide: function (target, room, user) {
-		if (!this.can('lock')) return false;
-		user.hiding = true;
-		user.updateIdentity();
-		this.sendReply("You have hidden your staff symbol.");
-	},
-
-	rk: 'kick',
-	roomkick: 'kick',
-	kick: function (target, room, user) {
-		if (!target) return this.parse('/help kick');
-		if (user.locked && !user.can('bypassall')) {
-			return this.sendReply("You cannot do this while unable to talk.");
-		}
-
-		target = this.splitTarget(target);
-		var targetUser = this.targetUser;
-		if (!targetUser || !targetUser.connected) return this.sendReply("User \"" + this.targetUsername + "\" not found.");
-		if (!this.can('mute', targetUser, room)) return false;
-
-		this.addModCommand(targetUser.name + " was kicked from the room by " + user.name + ".");
-		targetUser.popup("You were kicked from " + room.id + " by " + user.name + ".");
-		targetUser.leaveRoom(room.id);
-	},
-	kickhelp: ["/kick - Kick a user out of a room. Requires: % @ # & ~"],
 
 	masspm: 'pmall',
 	pmall: function (target, room, user) {
@@ -126,42 +58,6 @@ exports.commands = {
 	},
 	pmallstaffhelp: ["/pmallstaff [message] - Sends a PM to every staff member online."],
 
-	d: 'poof',
-	cpoof: 'poof',
-	poof: function (target, room, user) {
-		if (Config.poofOff) return this.sendReply("Poof is currently disabled.");
-		if (target && !this.can('broadcast')) return false;
-		if (room.id !== 'lobby') return false;
-		var message = target || messages[Math.floor(Math.random() * messages.length)];
-		if (message.indexOf('{{user}}') < 0) message = '{{user}} ' + message;
-		message = message.replace(/{{user}}/g, user.name);
-		if (!this.canTalk(message)) return false;
-
-		var colour = '#' + [1, 1, 1].map(function () {
-			var part = Math.floor(Math.random() * 0xaa);
-			return (part < 0x10 ? '0' : '') + part.toString(16);
-		}).join('');
-
-		room.addRaw("<strong><font color=\"" + colour + "\">~~ " + Tools.escapeHTML(message) + " ~~</font></strong>");
-		user.disconnectAll();
-	},
-	poofhelp: ["/poof - Disconnects the user and leaves a message in the room."],
-
-	poofon: function () {
-		if (!this.can('poofoff')) return false;
-		Config.poofOff = false;
-		return this.sendReply("Poof is now enabled.");
-	},
-	poofonhelp: ["/poofon - Enable the use /poof command."],
-
-	nopoof: 'poofoff',
-	poofoff: function () {
-		if (!this.can('poofoff')) return false;
-		Config.poofOff = true;
-		return this.sendReply("Poof is now disabled.");
-	},
-	poofoffhelp: ["/poofoff - Disable the use of the /poof command."],
-
 	regdate: function (target, room, user) {
 		if (!this.canBroadcast()) return;
 		if (!target || target === "0") target = toId(user.userid);
@@ -190,13 +86,6 @@ exports.commands = {
 		});
 	},
 	regdatehelp: ["/regdate - Please specify a valid username."],
-
-	show: function (target, room, user) {
-		if (!this.can('lock')) return false;
-		user.hiding = false;
-		user.updateIdentity();
-		this.sendReply("You have revealed your staff symbol.");
-	},
 
 	sb: 'showdownboilerplate',
 	showdownboilerplate: function (target, room, user) {
