c3a48f2be73d0d3d74390146d1a81f8db7949311
awolffromspace <ewolft@gmail.com>
2015-08-30 19:12:48 -0400

    Add /givetitle

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -319,6 +319,27 @@ exports.commands = {
 	},
 	resetsymbolhelp: ["/resetsymbol - Resets your custom symbol."],
 
+	givetitle: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target || target.indexOf(',') < 0) return this.parse('/help givetitle');
+
+		var parts = target.split(',');
+		var username = parts[0];
+		var title = parts[1];
+
+		if (typeof amount === 'string') return this.sendReply(amount);
+
+		var _this = this;
+		Database.read('title', toId(username), function (err, initial) {
+			if (err) throw err;
+			Database.write('title', title, toId(username), function (err, total) {
+				if (err) throw err;
+				_this.sendReply(username + " was given the profile title " + title + ".");
+			});
+		});
+	},
+	givetitlehelp: ["/givetitle [message] - Give a user a profile title."],
+
 	bplog: function (target, room, user, connection) {
 		if (!this.can('modlog')) return;
 		var numLines = 15;
