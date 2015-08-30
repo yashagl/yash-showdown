8ca80361161ecf706ef178cc6eb116dbd630a545
awolffromspace <ewolft@gmail.com>
2015-08-30 08:23:27 -0400

    Update pc-commands.js

--- chat-plugins/pc-commands.js
+++ chat-plugins/pc-commands.js
@@ -7,5 +7,17 @@ exports.commands = {
 			"- <a href=\"https://translate.google.com/#en/en/Official%20chat%20rooms%20are%20English%20only.%20Other%20languages%20are%20allowed%20in%20battle%20rooms%2C%20private%20messages%2C%20and%20unofficial%20chat%20rooms.\">TRANSLATION</a><br />" +
 			"- <a href=\"http://www.pokecommunity.com/showthread.php?t=289012#rules\">PC Battle Server Rules</a>"
 		);
+	},
+
+	ascii: function (target, room, user) {
+		if (!target) return;
+		if (!this.canBroadcast()) return;
+		var originalVersion = target;
+		var newVersion = target;
+		newVersion = newVersion.replace(/[^\x00-\x7F]/g, "?");
+		this.sendReplyBox(
+			"Original version: " + originalVersion + "<br />" +
+			"ASCII-only version: " + newVersion
+		);
 	}
 };
