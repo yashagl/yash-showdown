0abfe5bfa003d38621ac1d97b9525464cf77b525
awolffromspace <ewolft@gmail.com>
2015-09-03 13:20:36 -0400

    Add /autojoinroom

--- chat-plugins/pc-commands.js
+++ chat-plugins/pc-commands.js
@@ -9,6 +9,21 @@ exports.commands = {
 		);
 	},
 
+	autojoinroom: function (target, room, user) {
+		if (!this.can('makeroom')) return;
+		if (target === 'off') {
+			delete room.autojoin;
+			this.addModCommand("" + user.name + " removed this room from the autojoin list.");
+			delete room.chatRoomData.autojoin;
+			Rooms.global.writeChatRoomData();
+		} else {
+			room.autojoin = true;
+			this.addModCommand("" + user.name + " added this room to the autojoin list.");
+			room.chatRoomData.autojoin = true;
+			Rooms.global.writeChatRoomData();
+		}
+	},
+
 	ascii: function (target, room, user) {
 		if (!target) return;
 		if (!this.canBroadcast()) return;
