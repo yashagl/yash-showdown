2e004935ae07740f2da358a6a37c2f3c48ebec61
awolffromspace <ewolft@gmail.com>
2015-10-04 19:07:25 -0400

    Repurpose Furry Random to SwagPlay

--- mods/furryrandom/formats-data.js
+++ mods/furryrandom/formats-data.js
@@ -1,10 +1,17 @@
 exports.BattleFormatsData = {
-	purrloin: {
-		randomBattleMoves: ["foulplay","swagger","substitute","thunderwave"],
-		tier: "LC"
+	sableyemega: {
+		randomBattleMoves: ["recover","willowisp","darkpulse","calmmind","shadowball"],
+		randomDoubleBattleMoves: ["fakeout","knockoff","darkpulse","shadowball","willowisp","protect"],
+		tier: "OU"
 	},
-	liepard: {
-		randomBattleMoves: ["foulplay","swagger","substitute","thunderwave"],
-		tier: "NU"
-	}
+	banette: {
+		randomBattleMoves: ["destinybond","taunt","shadowclaw","suckerpunch","willowisp","shadowsneak","knockoff"],
+		randomDoubleBattleMoves: ["shadowclaw","suckerpunch","willowisp","shadowsneak","knockoff","protect"],
+		eventPokemon: [
+			{"generation":3,"level":37,"abilities":["insomnia"],"moves":["helpinghand","feintattack","shadowball","curse"]},
+			{"generation":5,"level":37,"gender":"F","isHidden":true,"moves":["feintattack","hex","shadowball","cottonguard"]}
+		],
+		requiredItem: "Banettite",
+		tier: "(RU)"
+	},
 };
