be60800eac2c53eaa9853edb20158cb19c5f0f2a
awolffromspace <ewolft@gmail.com>
2015-10-24 23:23:19 -0400

    Fix 1v1 formats

--- data/rulesets.js
+++ data/rulesets.js
@@ -259,6 +259,22 @@ exports.BattleFormats = {
 			}
 		}
 	},
+	teampreview1v1: {
+		effectType: 'Rule',
+		onStartPriority: -10,
+		onStart: function () {
+			this.add('clearpoke');
+			for (var i = 0; i < this.sides[0].pokemon.length; i++) {
+				this.add('poke', this.sides[0].pokemon[i].side.id, this.sides[0].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
+			}
+			for (var i = 0; i < this.sides[1].pokemon.length; i++) {
+				this.add('poke', this.sides[1].pokemon[i].side.id, this.sides[1].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
+			}
+		},
+		onTeamPreview: function () {
+			this.makeRequest('teampreview', 1);
+		}
+	},
 	teampreview: {
 		effectType: 'Rule',
 		onStartPriority: -10,
