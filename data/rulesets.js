4827135dbb6b67c0fb37d65111ca4b0c85c78c0f
awolffromspace <ewolft@gmail.com>
2015-10-29 16:39:35 -0400

    Fix Metronome 3v3 Random

--- data/rulesets.js
+++ data/rulesets.js
@@ -288,8 +288,23 @@ exports.BattleFormats = {
 			}
 		},
 		onTeamPreview: function () {
-			var lengthData = this.getFormat().teamLength;
-			this.makeRequest('teampreview', lengthData && lengthData.battle || '');
+			this.makeRequest('teampreview');
+		}
+	},
+	teampreviewgbu: {
+		effectType: 'Rule',
+		onStartPriority: -10,
+		onStart: function () {
+			this.add('clearpoke');
+			for (var i = 0; i < this.sides[0].pokemon.length; i++) {
+				this.add('poke', this.sides[0].pokemon[i].side.id, this.sides[0].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
+			}
+			for (var i = 0; i < this.sides[1].pokemon.length; i++) {
+				this.add('poke', this.sides[1].pokemon[i].side.id, this.sides[1].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
+			}
+		},
+		onTeamPreview: function () {
+			this.makeRequest('teampreview', 3);
 		}
 	},
 	littlecup: {
