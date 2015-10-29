b575ef76f92421d353d3eabd393a6e22d45d809b
awolffromspace <ewolft@gmail.com>
2015-10-29 15:14:39 -0400

    Fix custom kick permissions

--- commands.js
+++ commands.js
@@ -972,14 +972,14 @@ var commands = exports.commands = {
 	ck: 'customkick',
 	ckick: 'customkick',
 	customkick: function (target, room, user) {
-		if (!this.can('kick', targetUser, room)) return false;
 		if (!target) return;
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
-		if (targetUser.userid === user.userid) return this.sendReply('Please buy a poof from the "/shop" instead.');
 		if (!targetUser || !targetUser.connected) {
 			return this.sendReply("User " + this.targetUsername + " not found.");
 		}
+		if (targetUser.userid === user.userid) return this.sendReply('Please buy a poof from the "/shop" instead.');
+		if (!this.can('kick', targetUser, room)) return false;
 		var msg = (target ? " " + target + "" : "");
 		this.add(targetUser.name + msg);
 		this.logModCommand("" + targetUser.name + " was kicked by " + user.name + ".");
