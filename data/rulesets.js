d06f6acfb3ecba1dcbf904090740b0d2241d2fba
awolffromspace <ewolft@gmail.com>
2015-11-08 02:41:30 -0500

    Use let instead of var

--- data/rulesets.js
+++ data/rulesets.js
@@ -266,10 +266,10 @@ exports.BattleFormats = {
 		onStartPriority: -10,
 		onStart: function () {
 			this.add('clearpoke');
-			for (var i = 0; i < this.sides[0].pokemon.length; i++) {
+			for (let i = 0; i < this.sides[0].pokemon.length; i++) {
 				this.add('poke', this.sides[0].pokemon[i].side.id, this.sides[0].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
 			}
-			for (var i = 0; i < this.sides[1].pokemon.length; i++) {
+			for (let i = 0; i < this.sides[1].pokemon.length; i++) {
 				this.add('poke', this.sides[1].pokemon[i].side.id, this.sides[1].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
 			}
 		},
@@ -299,10 +299,10 @@ exports.BattleFormats = {
 		onStartPriority: -10,
 		onStart: function () {
 			this.add('clearpoke');
-			for (var i = 0; i < this.sides[0].pokemon.length; i++) {
+			for (let i = 0; i < this.sides[0].pokemon.length; i++) {
 				this.add('poke', this.sides[0].pokemon[i].side.id, this.sides[0].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
 			}
-			for (var i = 0; i < this.sides[1].pokemon.length; i++) {
+			for (let i = 0; i < this.sides[1].pokemon.length; i++) {
 				this.add('poke', this.sides[1].pokemon[i].side.id, this.sides[1].pokemon[i].details.replace(/(Arceus|Gourgeist|Genesect|Pumpkaboo)(-[a-zA-Z?]+)?/g, '$1-*'));
 			}
 		},
