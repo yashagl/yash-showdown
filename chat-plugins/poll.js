cf049deb70c474ff743b5f6e86de2c9ade358e48
awolffromspace <ewolft@gmail.com>
2015-11-08 23:58:46 -0500

    Add PU to tour polls

--- chat-plugins/poll.js
+++ chat-plugins/poll.js
@@ -143,12 +143,12 @@ exports.commands = {
 	tourneypoll: 'tourpoll',
 	tourpoll: function (target, room, user) {
 		if (!this.can('poll', null, room)) return false;
-		this.parse("/poll Tournament format?," + "OU, Ubers, UU, RU, NU, LC, VGC, Monotype, [Gen 4] OU, Random, 1v1 Random, Uber Random, High Tier Random, Low Tier Random, LC Random, Monotype Random, Generational Random, Inverse Random, Community Random, Orb Random, Hoenn Random, Hoenn Weather Random, Super Smash Bros. Random, Winter Wonderland, Super Staff Bros., SwagPlay Random, Metronome 3v3 Random, Doubles Random, Triples Random, Battle Factory, Hackmons Cup, [Seasonal] Random, [Gen 2] Random, [Gen 1] Random");
+		this.parse("/poll Tournament format?," + "OU, Ubers, UU, RU, NU, PU, LC, VGC, Monotype, [Gen 4] OU, Random, 1v1 Random, Uber Random, High Tier Random, Low Tier Random, LC Random, Monotype Random, Generational Random, Inverse Random, Community Random, Orb Random, Hoenn Random, Hoenn Weather Random, Super Smash Bros. Random, Winter Wonderland, Super Staff Bros., SwagPlay Random, Metronome 3v3 Random, Doubles Random, Triples Random, Battle Factory, Hackmons Cup, [Seasonal] Random, [Gen 2] Random, [Gen 1] Random");
 	},
 
 	teampoll: function (target, room, user) {
 		if (!this.can('poll', null, room)) return false;
-		this.parse("/poll Tournament format?," + "OU, Ubers, UU, RU, NU, LC, VGC, Monotype, [Gen 4] OU");
+		this.parse("/poll Tournament format?," + "OU, Ubers, UU, RU, NU, PU, LC, VGC, Monotype, [Gen 4] OU");
 	},
 
 	randbatpoll: 'randompoll',
