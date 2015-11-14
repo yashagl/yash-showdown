fa96fd40ec765e84fd43d9e3d898ab42f0ed75cc
awolffromspace <ewolft@gmail.com>
2015-11-13 19:50:59 -0500

    Update formats-data.js

--- mods/hightierrandom/formats-data.js
+++ mods/hightierrandom/formats-data.js
@@ -2,60 +2,76 @@
 
 exports.BattleFormatsData = {
 	charizard: {
+		inherit: true,
 		requiredItem: "Charizardite X"
 	},
 	beedrill: {
+		inherit: true,
 		requiredItem: "Beedrillite"
 	},
 	pidgeot: {
+		inherit: true,
 		requiredItem: "Pidgeotite"
 	},
 	gengarmega: {
 		tier: "Uber"
 	},
 	pinsir: {
+		inherit: true,
 		requiredItem: "Pinsirite"
 	},
 	aerodactyl: {
+		inherit: true,
 		requiredItem: "Aerodactylite"
 	},
 	ampharos: {
+		inherit: true,
 		requiredItem: "Ampharosite"
 	},
 	houndoom: {
+		inherit: true,
 		requiredItem: "Houndoominite"
 	},
 	sceptile: {
+		inherit: true,
 		requiredItem: "Sceptilite"
 	},
 	gallade: {
+		inherit: true,
 		requiredItem: "Galladite"
 	},
 	aggron: {
+		inherit: true,
 		requiredItem: "Aggronite"
 	},
 	medicham: {
+		inherit: true,
 		requiredItem: "Medichamite"
 	},
 	manectric: {
+		inherit: true,
 		requiredItem: "Manectite"
 	},
 	altaria: {
+		inherit: true,
 		requiredItem: "Altarianite"
 	},
 	absol: {
+		inherit: true,
 		requiredItem: "Absolite"
 	},
 	salamencemega: {
 		tier: "Uber"
 	},
 	lopunny: {
+		inherit: true,
 		requiredItem: "Lopunnite"
 	},
 	lucariomega: {
 		tier: "Uber"
 	},
 	abomasnow: {
+		inherit: true,
 		requiredItem: "Abomasite"
 	}
 };
