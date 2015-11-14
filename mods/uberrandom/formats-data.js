8a8af34a2ab7c6a2361c1890ce307a3db3b76131
awolffromspace <ewolft@gmail.com>
2015-11-13 19:53:36 -0500

    Update formats-data.js

--- mods/uberrandom/formats-data.js
+++ mods/uberrandom/formats-data.js
@@ -2,21 +2,26 @@
 
 exports.BattleFormatsData = {
 	gengar: {
+		inherit: true,
 		requiredItem: "Gengarite"
 	},
 	kangaskhan: {
+		inherit: true,
 		requiredItem: "Kangaskhanite"
 	},
 	mawile: {
+		inherit: true,
 		requiredItem: "Mawilite"
 	},
 	salamence: {
+		inherit: true,
 		requiredItem: "Salamencite"
 	},
 	rayquazamega: {
 		tier: "AG"
 	},
 	lucario: {
+		inherit: true,
 		requiredItem: "Lucarionite"
 	}
 };
